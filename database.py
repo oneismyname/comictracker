99eaa44039881fac56b2c09a5d904290edcfd488
thienan <thienanle2210@gmail.com>
2023-08-25 09:55:23 +0700

    test routes

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
-from flask import Flask
+
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
-db.init_app(app)
+db = SQLAlchemy()
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(1000), nullable=False)
