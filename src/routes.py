5e6bcc3428cba95cc37c7042d076eed2bfc99c29
thienan <thienanle2210@gmail.com>
2023-08-26 11:50:37 +0700

    successfully follow and unfollow

--- src/routes.py
+++ src/routes.py
@@ -69,37 +69,44 @@ def add():
     form = AddForm()
     if form.validate_on_submit():
         headers = {
-            'authority': 'manga.glhf.vn',
+            'authority': 'pb.tana.moe',
             'accept': '*/*',
-            'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
-            'referer': 'https://manga.glhf.vn/',
+            'accept-language': 'en-US',
+            'content-type': 'application/json',
+            'origin': 'https://tana.moe',
+            'referer': 'https://tana.moe/',
             'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
             'sec-fetch-dest': 'empty',
             'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
+            'sec-fetch-site': 'same-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
         }
 
-        response = requests.get(
-            f'https://manga.glhf.vn/api/releases?start={form.date_start.data}&end={form.date_end.data}&order=ascending&',
-            headers=headers,
-        )
+        params = {
+            'page': '1',
+            'perPage': '500',
+            'skipTotal': '1',
+            'filter': f"publishDate >= '{form.date_start.data}' && publishDate <= '{form.date_end.data}'",
+            'sort': '+publishDate,+name,-edition',
+            'expand': 'title, publisher',
+        }
+
+        response = requests.get('https://pb.tana.moe/api/collections/book_detailed/records', params=params,
+                                headers=headers)
         data_test = response.json()
-        for data in data_test:
+        for data in data_test['items']:
             with app.app_context():
-                info = data["entries"]
-                for item in info:
-                    name = item["name"].split(" - ")[0]
-                    result = db.session.execute(db.Select(Comic).where(Comic.name == name)).scalars().all()
-                    if result:
-                        pass
-                    else:
-                        new_comic = Comic(
-                            name=name)
-                        db.session.add(new_comic)
-                        db.session.commit()
+                name = data["name"].split(" - ")[0]
+                result = db.session.execute(db.Select(Comic).where(Comic.name == name)).scalars().all()
+                if result:
+                    pass
+                else:
+                    new_comic = Comic(
+                        name=name)
+                    db.session.add(new_comic)
+                    db.session.commit()
         return redirect(url_for("home"))
     return render_template('add.html', user=current_user, form=form)
 
@@ -112,24 +119,47 @@ def tracking():
             with app.app_context():
                 search = form.name.data
                 result = db.session.query(Comic).where(Comic.name.ilike(f'%{search}%')).all()
-                return render_template('tracking.html', form=form, user=current_user, comics=result)
+                follow_or_not = {}
+                for comic in result:
+                    query = Mapping.query.filter(Mapping.comic_id == comic.id, Mapping.user_id == current_user.id).all()
+                    if query:
+                        new_element = {comic.id: "TRUE"}
+                        follow_or_not.update(new_element)
+                    else:
+                        new_element = {comic.id: "FALSE"}
+                        follow_or_not.update(new_element)
+                return render_template('tracking.html', form=form, user=current_user, comics=result, dict = follow_or_not)
         else:
             flash("You have to login to add")
     return render_template("tracking.html", form=form, user=current_user)
 
 
-@app.route("/follow/<index>", methods=["GET", "POST"])
+@app.route("/follow/<index>")
 def follow(index):
     with app.app_context():
-        result = db.session.execute(db.Select(Mapping).where(Mapping.comic_id == index and Mapping.user_id == current_user.id)).scalars().all()
-        if result:
-            flash("you already follow this comic")
-            return redirect(url_for('tracking'))
-        else:
-            new_follow = Mapping(
-                user_id = current_user.id,
-                comic_id = index
-            )
-            db.session.add(new_follow)
-            db.session.commit()
-            return redirect(url_for('tracking'))
+        new_follow = Mapping(
+            user_id = current_user.id,
+            comic_id = index
+        )
+        db.session.add(new_follow)
+        db.session.commit()
+        flash("Successfully follow this comic")
+        return redirect(url_for('tracking'))
+
+
+@app.route("/unfollow/<index>")
+def unfollow(index):
+    with app.app_context():
+        query = Mapping.query.filter(Mapping.comic_id == index, Mapping.user_id == current_user.id).first()
+        db.session.delete(query)
+        db.session.commit()
+        flash("Successfully unfollow this comic")
+        return redirect(url_for('tracking'))
+
+
+@app.route("/profile")
+def profile():
+    with app.app_context():
+        query = db.session.query(Comic).join(Mapping).filter(Mapping.user_id == current_user.id).all()
+        print(query)
+        return render_template("profile.html", list = query, user=current_user)
